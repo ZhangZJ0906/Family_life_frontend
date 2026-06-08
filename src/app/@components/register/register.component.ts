5817b3228b3f0fbfc243f7d1ffa8479a882965d5
David <davids710119@gmail.com>
2026-06-08 12:21:08 +0800

    register驗證，但email重複偵測沒做

--- src/app/@components/register/register.component.ts
+++ src/app/@components/register/register.component.ts
@@ -9,6 +9,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import Swal from 'sweetalert2';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-register',
   imports: [CommonModule,
@@ -25,7 +27,8 @@ export class RegisterComponent {
 
   constructor(
     private readonly authService: AuthService,
-    private readonly router: Router
+    private readonly router: Router,
+    private http: HttpClient,
   ) {}
 
   name = '';
@@ -34,6 +37,8 @@ export class RegisterComponent {
   confirmPassword = '';
   avatar = '';
   is_notify: boolean = true;
+
+  emailVerified = false;
 
   get passwordError(): string {
     if (!this.password) {
@@ -60,33 +65,14 @@ export class RegisterComponent {
   }
 
   register(): void {
-    if (!this.email || !this.name || !this.password || !this.confirmPassword) {
-      Swal.fire({
-        icon: 'warning',
-        title: '資料未填完整',
-        text: '請完整填寫姓名、Email、密碼、確認密碼',
-        confirmButtonText: '確認',
-      });
-      return;
-    }
 
-    if (this.passwordError) {
-      Swal.fire({
-        icon: 'warning',
-        title: '密碼格式不正確',
-        text: this.passwordError,
-        confirmButtonText: '確認',
-      });
-      return;
-    }
+    if (!this.emailVerified) {
 
-    if (this.password !== this.confirmPassword) {
       Swal.fire({
         icon: 'warning',
-        title: '確認密碼錯誤',
-        text: '兩次輸入的密碼不一致',
-        confirmButtonText: '確認',
+        title: '請先完成Email驗證'
       });
+
       return;
     }
 
@@ -143,5 +129,124 @@ export class RegisterComponent {
     });
   }
 
+  startRegister(): void {
 
+    if (!this.email || !this.name || !this.password || !this.confirmPassword) {
+      Swal.fire({
+        icon: 'warning',
+        title: '資料未填完整',
+        text: '請完整填寫資料'
+      });
+      return;
+    }
+
+    if (this.passwordError) {
+      Swal.fire({
+        icon: 'warning',
+        title: '密碼格式不正確',
+        text: this.passwordError
+      });
+      return;
+    }
+
+    if (this.confirmPasswordError) {
+      Swal.fire({
+        icon: 'warning',
+        title: '確認密碼錯誤',
+        text: this.confirmPasswordError
+      });
+      return;
+    }
+
+    // 開始寄驗證碼
+    this.verifyEmailExist(this.email);
+  }
+
+  verifyEmailExist(email: string) {
+    Swal.fire({
+      title: '正在送驗證碼到你的gmail...',
+      allowOutsideClick: false,
+      didOpen: () => {
+        Swal.showLoading();
+      }
+    });
+    this.http.post(`http://localhost:8080/users/send?email=${email}`,{},
+    {
+      responseType: 'text'
+    }).subscribe({
+
+      next: (res: any) => {
+        Swal.close()
+        Swal.fire('驗證碼已送出', '', 'success');
+        this.showVerifyDialog();
+      },
+      error: (err) => {
+        Swal.close()
+        console.error(err);
+        Swal.fire('送出失敗', '', 'error');
+      }
+    });
+  }
+
+  showVerifyDialog(){
+    Swal.fire({
+      title: '輸入驗證碼',
+      input: 'text',
+      inputPlaceholder: '請輸入驗證碼',
+      showCancelButton: true,
+      confirmButtonText: '驗證',
+      cancelButtonText: '取消',
+
+      preConfirm: async (verifyCode) => {
+
+        if (!verifyCode) {
+          Swal.showValidationMessage('請輸入驗證碼');
+          return;
+        }
+
+        return verifyCode;
+      }
+
+    }).then((result) => {
+      // 按取消
+      if (!result.isConfirmed) {
+        return;
+      }
+
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: '驗證中...',
+          allowOutsideClick: false,
+          didOpen: () => {
+            Swal.showLoading();
+          }
+        });
+
+        this.http.post(
+          `http://localhost:8080/users/verify?email=${this.email}&code=${result.value}`,
+        {},
+        {
+          responseType: 'text'
+        }).subscribe({
+          next: (res: any) => {
+            Swal.close()
+            if(res == "驗證成功"){
+              Swal.fire('驗證成功', '', 'success');
+              // 驗證成功才真正註冊
+              this.register();
+            }
+            else{
+              Swal.fire('驗證失敗', '', 'error');
+            }
+          },
+
+        error: () => {
+          Swal.close()
+          Swal.fire('驗證失敗', '', 'error');
+        }
+      });
+
+    }
+   });
+  }
 }
